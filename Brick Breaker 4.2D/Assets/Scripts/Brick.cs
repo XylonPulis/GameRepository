e4a9632917c1defa9c69e68638ab32513edea6a8
Xylon Pulis <a100889@mcast.org.mt>
2017-11-17 14:18:44 +0100

    Added Level 2 Added Sound Effects Added New Hit Line

---
+++ Brick Breaker 4.2D/Assets/Scripts/Brick.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+    int timesHit;
+    LevelManager levelManager = new LevelManager();
+    public int MaxHit;
+	// Use this for initialization
+	void Start () {
+
+        timesHit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        timesHit++;
+
+        print(gameObject.name + " : " + timesHit);
+
+        if(timesHit >= MaxHit)
+        {
+            TestWin();
+            Destroy(gameObject);
+        }   
+    }
+        void TestWin()
+
+        {
+            levelManager.LoadNextScene();
+        }
+
+}
